1fa2e3164abdf48b28f4cc04d34a036badb26b0b
jamespeterson <jamespeterson0918@yahoo.com>
2017-10-13 16:31:10 -0400

    finished seeds and schema

---
+++ db/schema.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+
+const reviewSchema = mongoose.Schema({
+    reviewTitle: String,
+    date: String, // For mock log-in. Do not enter actual passwords
+})
+
+const movieSchema = mongoose.Schema({
+  title: {
+    type: String,
+    default: 'New Review'
+  },
+  description: {type: String, default: 'New Movie Review'},
+  createdAt: {type: Date, default: Date.now},
+  reviews: [reviewSchema]
+})
+
+const userSchema = mongoose.Schema({
+    userName: String,
+    company: String, // For mock log-in. Do not enter actual passwords
+})
+
+const Movie = mongoose.model('Movie', movieSchema)
+const User = mongoose.model('User', userSchema)
+const Review = mongoose.model('Review', reviewSchema)
+
+module.exports = {
+  Movie, User, Review
+}
---
+++ db/seeds.js
@@ -0,0 +1,53 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true})
+mongoose.Promise = global.Promise
+
+const { User, Movie, Review} = require('./schema')
+
+const americanHistoryXReview1 = new Review({
+    title: 'American History X movie review',
+    tagline: 'Some Legacies Must End.',
+    review: 'Edward Norton gives a blistering and memorable performance.'
+})
+
+const titanicReview1 = new Review({
+    title: 'Titanic movie review',
+    tagline: 'Nothing On Earth Could Come Between Them.',
+    review: 'The role of Leonardo Dicaprios career.'
+})
+
+const theTownReview1 = new Review({
+    title: 'The Town movie review',
+    tagline: 'Welcome To The Bank Robbery Capital Of America.',
+    review: 'Ben Affleck and Jeremy Renner turn in the role of a lifetime.'
+})
+
+const titanic = new Movie({
+  title: 'Titanic',
+  description: "Two passengers fall in love on the ill fated ship."
+})
+const americanHistoryX = new Movie({
+  title: 'American History X',
+  description: "An older brother tries to stop his younger brother from following in his footsteps.",
+    reviews: [americanHistoryXReview1]
+})
+
+const theTown = new Movie({
+    title: 'The Town',
+    description: "Doug MacRay leads a gang of bank robbers in Boston."
+  })
+
+const jasonVoorhees = new User({
+  userName: 'jason_voorhees',
+})
+
+// Uses promises to make sure remove is run first, then saves new user.
+User.remove({})
+  .then(() => jasonVoorhees.save())
+  .then(() => console.log('User Saved'))
+Movie.remove({})
+    .then(() => titanic.save())
+    .then(() => theTown.save())
+    .then(() => americanHistoryX.save())
+    .then(() => mongoose.connection.close())
