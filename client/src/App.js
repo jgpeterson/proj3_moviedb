62a09e63029b089ff24b788f1102f737e1fe9b06
jamespeterson <jamespeterson0918@yahoo.com>
2017-10-16 09:22:23 -0400

    edited app js in src

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,26 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+        <div>
+        <div>
+          <Link to="/">Home</Link>
+        </div>
+        <div>
+          <Link to="/user">UserPage</Link>
+        </div>
+        <div>
+        <Link to="/review">Reviews</Link>
+        </div>
+        </div>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/user" component={UserPage} />
+          <Route exact path="/review/:userId" component={IdeaPage} />
+        </Switch>
+        </div>
+        </Router>
     );
   }
 }
